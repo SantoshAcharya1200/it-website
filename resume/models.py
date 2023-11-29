b0830f6346020f710191021ff973ee382c089d0a
Santosh Acharya <41406942+SantoshAcharya1200@users.noreply.github.com>
2023-11-29 14:05:04 +0545

    Delete resume directory

--- resume/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Destination(models.Model):
-
-    img = models.ImageField(upload_to="pics")
-    des = models.CharField(max_length=400)
-    title=  models.CharField(max_length=100)
